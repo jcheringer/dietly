bb2464ed67267e143aba41b6aa3ba570e15fd7cb
João Heringer <jheringer@avenuecode.com>
2020-02-10 19:24:16 -0300

    Adjusting Login Page

--- src/frontend/app/pages/login/login-page.js
+++ src/frontend/app/pages/login/login-page.js
@@ -2,9 +2,11 @@ import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
-
 import AuthService from '../../service/auth-service';
 import { googleLogin } from '../../store/actions/users-action'
+
+import Style from './login-page.less';
+import CS from '../../../style/common.less';
 
 const loginPage = (props) => {
     const googleApi = window.gapi;
@@ -21,6 +23,10 @@ const loginPage = (props) => {
     const [isGoogleReady, setGoogleReady] = useState(false);
 
     const googleLoginHandler = () => {
+        if (!isGoogleReady) {
+            return;
+        }
+
         const googleAuth = googleApi.auth2.getAuthInstance();
 
         googleAuth.signIn().then(googleUser => {
@@ -41,11 +47,33 @@ const loginPage = (props) => {
     }, []);
 
     return (
-        <div>
-            <h1>Login Page</h1>
-            { isGoogleReady && (
-                <button onClick={ googleLoginHandler }>Google Login</button>
-            ) }
+        <div className={ Style.LoginPage }>
+            <h1>Dietly</h1>
+            <div className={ Style.Container }>
+                <div className={ Style.Content }>
+                    <div className={ CS.FloatingLabelContainer }>
+                        <input type="text" placeholder="E-mail" />
+                        <label>E-mail</label>
+                    </div>
+                    <div className={ CS.FloatingLabelContainer }>
+                        <input type="text" placeholder="Senha" />
+                        <label>Senha</label>
+                    </div>
+                    <button className={ [CS.BtnPrimary, CS.BlockButton].join(' ') }>Login</button>
+                    <div className={ Style.LabeledDiv }>
+                        <hr />
+                        <span>Ou</span>
+                        <hr />
+                    </div>
+                    <button
+                        onClick={ googleLoginHandler }
+                        className={ [Style.GoogleButton, CS.BlockButton].join(' ') }
+                    >
+                        <span className={ Style.Icon } />
+                        <span>Entre com o Google</span>
+                    </button>
+                </div>
+            </div>
         </div>
     )
 };
